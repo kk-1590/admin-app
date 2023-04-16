b9644c9ef060e53c35fe605f2c299d33da9fb8a8
kanakkhandelwal25~ <kkhandelwal1590@gmail.com>
2023-04-16 21:55:30 +0530

    added chart and tables

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,11 @@ import React from 'react'
 import './Home.scss';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
+import Widget from '../../components/widget/Widget';
+import Featured from '../../components/featured/Featured';
+import Chart from '../../components/chart/Chart';
+// import { Table } from '@mui/material';
+import Table from '../../components/table/Table';
 
 const Home = () => {
   return (
@@ -9,7 +14,20 @@ const Home = () => {
         <Sidebar />
         <div className='homeContainer'>
           <Navbar />
-          Home container
+          <div className='widgets'>
+            <Widget type = "user" />
+            <Widget type = "order" />
+            <Widget type = "earning" />
+            <Widget type = "balance" />
+          </div>
+          <div className='charts'>
+            <Featured />
+            <Chart />
+          </div>
+          <div className='listContainer'>
+            <div className='listTitle'>Latest Transactions</div>
+            <Table />
+          </div>
         </div>
     </div>
   )
